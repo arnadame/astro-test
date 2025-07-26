989b6030b4011e346d2ab21875e962af37a673f6
arnadame <adameaaron25@gmail.com>
2025-07-26 15:06:20 +0800

    added light/dark mode

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -3,15 +3,17 @@ import { useState } from 'preact/hooks';
 export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
+  
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+    <div class="flex flex-col items-center">
+      <h3 class="text-xl py-5">{greeting}! Thank you for visiting!</h3>
+      {/* <button >
         New Greeting
-      </button>
+      </button> */}
+      <button onClick={() => setGreeting(randomMessage())} type="button" class="text-gray-900 cursor-pointer bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600">New Greeting</button>
+
     </div>
   );
 }
